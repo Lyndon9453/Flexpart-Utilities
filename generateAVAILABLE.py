4237af0f09d3f1466c72e4c0a7baac1fe4c7c392
Mauro Gonzalez <maurogonzalez123@gmail.com>
2020-08-05 13:56:48 -0300

    Update generateAVAILABLE.py

--- generateAVAILABLE.py
+++ generateAVAILABLE.py
@@ -1,61 +1,69 @@
-#!/usr/bin/python3
+##!/usr/bin/env python
+
 """ Generate AVAILABLE file for meteo files in METEO_DIR.
-	AVAILABLE is written to METEO_DIR, so script can be called from anywhere.
-	Only meteo files valid for METEO_PREFIX are included.
+   AVAILABLE is written to METEO_DIR, so script can be called from anywhere.
+   Only meteo files valid for METEO_PREFIX are included.
 """
 
 import glob
 import os
 import sys
 
-#METEO_DIR = "/eunadics_data/mmulder/GV_ecm_0p2/"
-#METEO_DIR = "/eunadics_data/mmulder/eyja/ecm_data/"
-#METEO_DIR = "/eunadics_data/mmulder/gfs_gv/"
-METEO_DIR = "/home/mgonzalez/Descargas/fptraining2019/flexpart_v10.3.1_3cd0f17/preprocess/flex_extract/workspace"
-
-# prefix in regex format
-#METEO_PREFIX = "EN*"
-#METEO_PREFIX = "fnl_*"
-METEO_PREFIX = "EA*"
-
-#For DUST:
-#METEO_PREFIX = METEO_DIR+"EN*"
-
-# Test if dir exists
-if os.path.isdir(METEO_DIR):
-  os.chdir(METEO_DIR)
+if len(sys.argv) >= 2:
+    METEO_PREFIX = sys.argv[1]
 else:
-  print("\nDirectory does not exist.")
-  sys.exit()
+    print("Insert the prefix of the meteo files (characters before the datetime numbers)\n" \
+          "You must insert the path where meteo files are, otherwise yo must execute this script\n" \
+          "from the folder with meteo files.\n" \
+          "Example of usage:\n" \
+          "\t\tpython generateAVAILABLE.py --prefix --path\n"
+          "\t\tpython generateAVAILABLE.py EApub /home/maxpower/flexpart/preprocess/wind_fields/\n"
+          "Aborting Execution")
+    sys.exit()
 
+if len(sys.argv) >= 3:
+    METEO_DIR = sys.argv[2]
+    if os.path.isdir(METEO_DIR):
+        os.chdir(METEO_DIR)
+    else:
+        print("\nDirectory does not exist.\n"
+              "Aborting Execution")
+        sys.exit()
+else:
+    METEO_DIR = "."
+
+print ("Looking for METEO files with \"{}\" prefix in \"{}\" dir".format(METEO_PREFIX, os.getcwd()))
 # Load all files and sort as in AVAILABLE
-meteo_files = glob.glob(METEO_PREFIX)
+meteo_files = glob.glob(METEO_PREFIX + '*')
 meteo_files.sort()
 
 # Check if any meteo file exists
 if not meteo_files:
-  print("\nDirectory does not contain meteo files.")
-  sys.exit()
+    print("\nDirectory does not contain meteo files.\nPlease execute this script from"
+          " the folder with meteo files or insert the path where meteo files are.\n"
+          "Aborting Execution ")
+    sys.exit()
 
-#AVAILABLE = open('AVAILABLE', 'w')
-AVAILABLE = open(METEO_DIR+'/AVAILABLE', 'w')
-AVAILABLE.write("-\n-\n-\n")
 
+try:
+    "for LINUX's users"
+    AVAILABLE = open(os.getcwd() + '/AVAILABLE', 'w')
+except:
+    "for WINDOWS's users"
+    AVAILABLE = open(METEO_DIR + '\\AVAILABLE', 'w')
+
+# AVAILABLE.write("-\n-\n-\n")
+AVAILABLE.write(  """DATE     TIME        FILENAME             SPECIFICATIONS\
+   \nYYYYMMDD HHMISS\
+  \n________ ______      __________________    __________________\n""")
+print("\nData to be write in AVAILABLE file:\n")
 for meteo_file in meteo_files:
-#    print meteo_file
-#    print meteo_file[20]
-    # Sample line format "20130802 000000      EN13080200"
-     DATO = "20%s %s0000      %s      ON DISK\n" % (meteo_file[2:8], meteo_file[8:10], meteo_file)
-     AVAILABLE.write(DATO)
-     #print(DATO)
-#    AVAILABLE.write("20%s %s0000      %s\n" % (meteo_file[2:8], meteo_file[8:10], meteo_file)) # for ecm 0p2
-#    AVAILABLE.write("20%s %s0000      %s\n" % (meteo_file[11:17], meteo_file[18:20], meteo_file)) # for gfs fnl
-#    AVAILABLE.write("20%s %s0000      %s\n" % (meteo_file[11:17], str(int(meteo_file[18:20])+int(meteo_file[25])), meteo_file)) # for gfsanl
-#    print meteo_file[11:17]
-#    print meteo_file[18:20]
-#    print "0"+str(int(meteo_file[18:20])+int(meteo_file[25])) if(int(meteo_file[18:20])+int(meteo_file[25]))<12 else str(int(meteo_file[18:20])+int(meteo_file[25]))
-#   AVAILABLE.write("20%s %s0000      %s\n" % (meteo_file[11:17], "0"+str(int(meteo_file[18:20])+int(meteo_file[25])) if(int(meteo_file[18:20])+int(meteo_file[25]))<12 else str(int(meteo_file[18:20])+int(meteo_file[25])), meteo_file)) # for gfsanl
-
+    date = meteo_file[len(METEO_PREFIX):len(METEO_PREFIX)+6]
+    hour = meteo_file[len(METEO_PREFIX)+6:len(METEO_PREFIX)+8]
+    name = meteo_file
+    DATO = "20%s %s0000      %s      ON DISK\n" % (date, hour, name)
+    AVAILABLE.write(DATO)
+    print(DATO)
 
 AVAILABLE.close()
 
